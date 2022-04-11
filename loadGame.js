98d21590f8fcefac7991a8a321e8066b344e5372
AnikaMinali <100616048+AnikaMinali@users.noreply.github.com>
2022-04-11 17:58:51 -0400

    added more backgrounds and sprites

--- loadGame.js
+++ loadGame.js
@@ -4,11 +4,15 @@ class LoadGame extends Phaser.Scene {
   }
 
   preload(){
-    this.load.image("background", "assets/images/background.png");
-    this.load.image("woodendoor", "assets/images/woodendoor.png");
+    this.load.image("friendRoomBG", "assets/images/friendRoom.png");
+    this.load.image("tenthFloorBG", "assets/images/tenthFloor.png");
+    this.load.image("elevatorBG", "assets/images/insideElevator.png");
+    this.load.image("cityBG", "assets/images/cityBackground.png");
+    this.load.image("stairsAndElevatorBG", "assets/images/stairsElevator.png");
+    this.load.image("stairsBG", "assets/images/stairs.png");
     this.load.image("grass", "assets/images/grass.png");
     
-    this.load.spritesheet("player", "assets/spritesheets/power-up.png",{
+    this.load.spritesheet("player", "assets/spritesheets/youSprite.png",{
       frameWidth: gameSettings.playerSize,
       frameHeight: gameSettings.playerSize
     });
@@ -28,18 +32,38 @@ class LoadGame extends Phaser.Scene {
       frameHeight: gameSettings.playerSize
     });
     this.load.spritesheet("door1", "assets/images/woodendoor.png",{
+      frameWidth: gameSettings.playerSize/2,
+      frameHeight: gameSettings.playerSize/2
+    });  
+
+//new sprites
+/*     this.load.spritesheet("theCar", "assets/spritesheets/carSprite.png",{
+      frameWidth: gameSettings.playerSize*2,
+      frameHeight: gameSettings.playerSize*2
+    }); */
+     this.load.spritesheet("concernedMom", "assets/spritesheets/momSprite.png",{
+      frameWidth: gameSettings.playerSize,
+      frameHeight: gameSettings.playerSize
+    }); 
+    this.load.spritesheet("creepyDude", "assets/spritesheets/creepSprite.png",{
       frameWidth: gameSettings.playerSize,
       frameHeight: gameSettings.playerSize
     });
-
+    this.load.spritesheet("creepyDude2", "assets/spritesheets/creep2Sprite.png",{
+      frameWidth: gameSettings.playerSize,
+      frameHeight: gameSettings.playerSize
+    });
   }
 
   create() {
-    this.add.text(20, 20, "Loading game...");
 
+    this.add.text(20, 20, "Loading game...");
     //add death gifs and animations here
-    
     this.scene.start("mainMenu");
+
+    //Align.scaleToGameW(background, .1);
+
+
     
   }
 }
