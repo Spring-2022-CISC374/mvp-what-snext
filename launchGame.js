3d69f03300926f7c427afea3cc3d80c4de8afb51
AnikaMinali <100616048+AnikaMinali@users.noreply.github.com>
2022-04-14 05:33:52 -0400

    Update launchGame.js

--- launchGame.js
+++ launchGame.js
@@ -12,47 +12,58 @@ var config = {
     }
   }
 }
-
-if (config.width > config.height){
-  config.width = config.height;
-} else {
-  config.height = config.width;
-}
+config.width = 960;
+config.height = 640;
+//if (config.width > config.height){
+ // config.width = config.height;
+//} else {
+//  config.height = config.width;
+//}
 //TODO: Its a square, width and height are the same
+
 var locations = {
   left: config.width / 10,
   right: config.width - config.width / 10,
-  mid: config.height / 2,
+  furtherRight: config.width  - config.width / 15,
+  midHeight: config.height / 2,
   midLowerHeight: config.height / 1.5,
   lowHeight: config.height / 1.2,
-  lowerHeight: config.height / 1.1,
-  midLeft: config.width / 2.05,
-  midRight: config.width / 1.95,
+  lowerHeight: config.height / 1.15,
+  lowestHeight: config.height / 1,
+  midWidth: config.width / 2,
+  midWidthLeft: config.width / 2.05,
+  midWidthSlightRight: config.width / 1.95,
+  midWidthRight: config.width / 1.45,
   niceWidth: config.width - (config.width / 5),
-  top: config.height / 10,
-  oneThird: config.width / 3
+  top: config.height / 10
 }
 
 var phone = new npc("phone", "assets/spritesheets/phoneSprite.png", []);
-var crowd1 = new npc("crowd1", "assets/spritesheets/crowd1Sprite.png", ["I could try to capture their attention, so he leaves"]);
-var crowd2 = new npc("crowd2", "assets/spritesheets/crowd2Sprite.png", ["I could try to capture their attention, so he leaves"]);
-var crowd3 = new npc("crowd3", "assets/spritesheets/crowd3Sprite.png", ["I could try to capture their attention, so he leaves"]);
-var car = new npc("car", "assets/spritesheets/carSprite.png", []);
-var concernedMom = new npc("concernedMom", "assets/spritesheets/momSprite.png", ['CONCERNED MOM: I charged your phone, remember to call me if you need anything.', "YOU: Ill be fine mom",'CONCERNED MOM: I also packed your backpack with everything I thought you might need.', 'Click on an item and then click the button to use it.', 'Why is there a brick in here?!', 'CONCERNED MOM: I know I may have gone overboard, but its your first sleepover and I just want you to be prepared.', 'YOU: Dont worry, Im not a little kid anymore.']);
+var people = new npc("people", "assets/spritesheets/peopleSprite.png", ["I could try to capture their attention, so he leaves"]);
+var concernedMom = new npc("concernedMom", "assets/spritesheets/momSprite.png", ['CONCERNED MOM: I charged your phone, call me if you need anything.', "YOU: Ill be fine mom.",'CONCERNED MOM: Just be careful, Honey.','CONCERNED MOM: Remember to use your arrow keys to move.', 'YOU: I know how to cross the street, Mom.', 'CONCERNED MOM: I know, but its your first sleepover.','CONCERNED MOM: I want you to be prepared.', 'YOU: Dont worry, Im not a little kid anymore.', 'CONCERNED MOM: Just promise me that you will click on things if you ever need help.', 'YOU: I promise.', 'CONCERNED MOM: You can even click on people!!', 'YOU: Bye Mom!!']);
 var creepyDude2 = new npc("creepyDude","assets/spritesheets/creep2Sprite.png",['Who is this guy?', "Why is he so close?", "He's been following me ever since I left Mom", "I better get out quick"]);
-var creepyDude = new npc("creepyDude2","assets/spritesheets/creep2Sprite.png",["He's still here, this is getting weird.", "What should I do? \n**Go over to crowd and start yelling \n**call Mom \n**pretend not to notice and walk to bestie's house"]);
+var creepyDude = new npc("creepyDude2","assets/spritesheets/creep2Sprite.png",["He's still here, this is getting weird.", "What should I do? \n\n**-> Go over to crowd and start yelling. \n**-> Call Mom. \n**-> Pretend not to notice and walk to bestie's house."]);
+var girl = new npc("girl","assets/spritesheets/girlSprite.png", []);
+var boy = new npc("boy","assets/spritesheets/boySprite.png", []);
 
-
-var stairs = new Room("stairsBG",{},{});
-var elevator2 = new Room("elevatorBG",{},{});
-var stairsAndElevator = new Room("stairsAndElevatorBG",{stairs:[locations.left, locations.mid,this.stairs], elevator2:[locations.right, locations.mid,this.elevator2]});
-var tenthFloor2 = new Room("tenthFloorBG",{stairsAndElevator:[locations.right, locations.mid,this.stairsAndElevator]});
-var friendRoom = new Room("friendRoomBG",{tenthFloor2:[locations.right, locations.mid,this.tenthFloor2]});
-var tenthFloor = new Room("tenthFloorBG",{friendRoom:[locations.left, locations.mid,this.friendRoom]},{creepyDude:[locations.right,locations.midLowerHeight,this.creepyDude]}, {crowd1:[locations.mid,locations.mid,this.crowd1]}, {crowd2:[locations.midRight,locations.mid,this.crowd2]}, {crowd3:[locations.midLeft,locations.mid,this.crowd3]});
-var elevator = new Room("elevatorBG",{tenthFloor:[locations.left, locations.mid,this.tenthFloor]},{creepyDude2:[locations.mid,locations.lowHeight,this.creepyDude2]});
+var stairs = new Room("stairsBG",{},{phone:[locations.furtherRight, locations.top,this.phone]});
+var elevator2 = new Room("elevatorBG",{},{phone:[locations.furtherRight, locations.top,this.phone]});
+var stairsAndElevator = new Room("stairsAndElevatorBG",{stairs:[locations.midWidthLeft, locations.midHeight,this.stairs], elevator2:[locations.right, locations.midHeight,this.elevator2]}, {phone:[locations.furtherRight, locations.top,this.phone]});
+var tenthFloor2 = new Room("tenthFloorBG",{stairsAndElevator:[locations.right, locations.midHeight,this.stairsAndElevator]},{phone:[locations.furtherRight, locations.top,this.phone]});
+var friendRoom = new Room("friendRoomBG",{tenthFloor2:[locations.right, locations.midHeight,this.tenthFloor2]},{phone:[locations.furtherRight, locations.top,this.phone], boy:[locations.left, locations.lowHeight,this.boy]});
+var tenthFloor = new Room("tenthFloorBG",{friendRoom:[locations.left, locations.midHeight,this.friendRoom]},{creepyDude:[locations.furtherRight,locations.midLowerHeight,this.creepyDude], people:[locations.midWidthRight,locations.midHeight,this.people],phone:[locations.furtherRight, locations.top,this.phone]});
+var elevator = new Room("elevatorBG",{tenthFloor:[locations.left, locations.midHeight,this.tenthFloor]},{creepyDude2:[locations.midWidthSlightRight,locations.lowerHeight,this.creepyDude2],phone:[locations.furtherRight, locations.top,this.phone]});
 var city = new Room("cityBG"
-  ,{elevator:[locations.left, locations.mid,this.elevator]}
-  ,{concernedMom:[locations.niceWidth, locations.lowHeight, this.concernedMom],car:[locations.mid, locations.lowerHeight, this.car]});
+  ,{elevator:[locations.left, locations.midHeight,this.elevator]}
+  ,{concernedMom:[locations.niceWidth, locations.lowHeight, this.concernedMom],car:[locations.midWidth, locations.lowHeight, this.car],phone:[locations.furtherRight, locations.top,this.phone]});
+//var characterSelect = new Room("selectionBG",{city:[locations.midWidth,locations.lowHeight,this.city]},{girl:[locations.left, locations.midHeight,this.girl],boy:[locations.left, locations.midHeight,this.boy]});
+
+
+
+
+
+
+
 
 var gameSettings = {
   //All numbers here
