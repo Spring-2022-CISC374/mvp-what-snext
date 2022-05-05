985c3ff10ea769adc4078591957c2f951a99202e
AnikaMinali <100616048+AnikaMinali@users.noreply.github.com>
2022-05-05 09:18:39 -0400

    Update mainMenu.js

--- mainMenu.js
+++ mainMenu.js
@@ -19,13 +19,23 @@ class MainMenu extends Phaser.Scene {
       }
 
       this.BGM= this.sound.add("BGM");
-
-      this.titleMusic.play(musicConfig);
+      //const musicButton = this.add.text(110, 80, 'Play music').on('pointerdown', () => this.titleMusic.play(musicConfig));
+      //this.titleMusic.play(musicConfig);
       this.add.text(20, 20, "Main Menu");
       this.background = this.add.tileSprite(0,0,config.width,config.height,"titleScreenBG");
       this.background.setOrigin(0,0);
       this.title = this.add.image(450,300,"title");
-      const button = this.add.text(110, 80, 'Press Here to Play')
+
+      const musicButton = this.add.text(75, 130, 'Play Music')
+      .setOrigin(0.5)
+      .setPadding(10)
+      .setStyle({ backgroundColor: '#111' })
+      .setInteractive({ useHandCursor: true })
+      .on('pointerdown', () => this.titleMusic.play(musicConfig))
+      .on('pointerover', () => playButton.setStyle({ fill: '#f39c12' }))
+      .on('pointerout', () => playButton.setStyle({ fill: '#FFF' }));
+      
+      const playButton = this.add.text(110, 80, 'Press Here to Play')
             .setOrigin(0.5)
             .setPadding(10)
             .setStyle({ backgroundColor: '#111' })
@@ -33,8 +43,8 @@ class MainMenu extends Phaser.Scene {
             .on('pointerdown', () => this.scene.start("playGame"))
             .on('pointerdown', () => this.titleMusic.stop())
             .on('pointerdown', () => this.BGM.play(musicConfig))
-            .on('pointerover', () => button.setStyle({ fill: '#f39c12' }))
-            .on('pointerout', () => button.setStyle({ fill: '#FFF' }));
+            .on('pointerover', () => playButton.setStyle({ fill: '#f39c12' }))
+            .on('pointerout', () => playButton.setStyle({ fill: '#FFF' }));
         
       this.spacebar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
 
@@ -43,10 +53,10 @@ class MainMenu extends Phaser.Scene {
 
     update() {
         this.background.tilePositionX += 0.6;
-        if (Phaser.Input.Keyboard.JustDown(this.spacebar)){
+/*         if (Phaser.Input.Keyboard.JustDown(this.spacebar)){
           this.titleMusic.stop();
           this.scene.start("playGame");        
-        }
+        } */
     }
     
   }
